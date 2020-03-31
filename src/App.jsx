82192750dfdfccee8a9be41aed94e3190c7da3e0
FIlipp <filippkotenko@outlook.com>
2020-03-31 15:49:03 +0300

    new2

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import TodoListTasks from "./TodoListTasks";
+import TodoListFooter from "./TodoListFooter ";
+import TodoListHeader from "./TodoListHeader";
+
+
+class App extends React.Component {
+    render = () => {
+        return (
+            <div className="App">
+                <div className="todoList">
+                    <TodoListHeader />
+                    <TodoListTasks />
+                    <TodoListFooter />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
